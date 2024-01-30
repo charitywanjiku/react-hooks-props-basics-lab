9111210ce9966d3636a57d35babc47bc73e33a1e
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-01-30 16:17:01 +0300

    ASSIGNMENT done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links />
+        <a href="https://github.com/liza">https://github.com/liza</a>
+        <a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/</a>
       {/* add your <Links /> component here */}
+      <Links />
+      <Links />
     </div>
   );
 }
